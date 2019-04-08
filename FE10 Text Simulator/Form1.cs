2e22588800804727b3472c27dff10a86b83aee64
Fudgenuggets <37097880+Fudgenuggets@users.noreply.github.com>
2019-04-07 21:43:43 -0700

    Add project files.

---
+++ FE10 Text Simulator/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FE10_Text_Simulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
